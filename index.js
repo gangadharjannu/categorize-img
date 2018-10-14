d28b7a8b05a563bc7d90cdcc47dbd9fb476c9fa4
gangadharjannu <gangadhar014@gmail.com>
2018-10-14 21:50:06 +0530

    Added error handling. Added CLI arguments for passing source and destination directories.

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const path = require('path');
 const fs = require('fs');
 const http = require('http');
 
+// error handling
+const args = process.argv.slice(2);
+if (args.length !== 2) {
+    throw new Error("Please pass source and destination folders to continue running the script.");
+} else if (!(fs.existsSync(args[0]) || fs.existsSync(args[1]))) {
+    throw new Error("Please pass valid already existed source and destination folders to continue running the script.");
+}
+const sourceDirectory = args[0];
+const destinationDirectory = args[1];
+
 // https://stackoverflow.com/questions/29307768/using-regex-to-match-date-format-in-yyyymmdd
 const DATE_REGEX = /(?<!\d)(?:(?:20\d{2})(?:(?:(?:0[13578]|1[02])31)|(?:(?:0[1,3-9]|1[0-2])(?:29|30)))|(?:(?:20(?:0[48]|[2468][048]|[13579][26]))0229)|(?:20\d{2})(?:(?:0?[1-9])|(?:1[0-2]))(?:0?[1-9]|1\d|2[0-8]))(?!\d)/;
 
@@ -57,10 +67,10 @@ const parseDate = (str) => {
     const d = str.substr(6, 2);
     const D = new Date(y, m, d);
     const locale = "en-us";
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        month = D.toLocaleString(locale, {
-            month: "long"
-        });
+    const month = D.toLocaleString(locale, {
+        month: "long"
+    });
+
     if (D.getFullYear() == y && D.getMonth() == m && D.getDate() == d) {
         return `${D.getFullYear()}${path.sep}${D.toLocaleString(locale, {
             month: "long"
@@ -70,16 +80,18 @@ const parseDate = (str) => {
 }
 
 // get list of files having valid dates
-const files = walkSync('E:\me')
+const files = walkSync(sourceDirectory)
     .map(file => ({
         filePath: file,
         fileDatePath: parseDate(DATE_REGEX.exec(file) ? DATE_REGEX.exec(file)[0] : "")
     }))
     .filter(file => file.fileDatePath);
 
+// creating directory strcutre
+// moving files from src to dest
 files.forEach(file => {
-    mkDirByPathSync(`E:${path.sep}album${path.sep}${file.fileDatePath}`);
-    moveFiles(file.filePath, `E:${path.sep}album${path.sep}${file.fileDatePath}${path.sep}${path.basename(file.filePath)}`);
+    mkDirByPathSync(`${path.resolve(destinationDirectory)}${path.sep}${file.fileDatePath}`);
+    moveFiles(file.filePath, `${path.resolve(destinationDirectory)}${path.sep}${file.fileDatePath}${path.sep}${path.basename(file.filePath)}`);
 });
 
 http.createServer((req, res) => {
