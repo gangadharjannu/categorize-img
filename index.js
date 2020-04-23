b357059a346c2643d9a8d865b405cf5d011d1a02
gangadhar <gangadhar014@gmail.com>
2020-04-23 07:37:35 +0200

    Improves error handling, logging progress of whats happening

--- index.js
+++ index.js
@@ -7,23 +7,41 @@ const utils = require('./utils');
 // error handling
 const args = process.argv.slice(2);
 if (args.length !== 2) {
-    throw new Error('Please pass source and destination folders to continue running the script.');
+  throw new Error('Please pass source and destination folders to continue running the script.');
 } else if (!(fs.existsSync(args[0]) || fs.existsSync(args[1]))) {
-    throw new Error('Please pass valid already existed source and destination folders to continue running the script.');
+  throw new Error('Please pass valid already existed source and destination folders to continue running the script.');
 }
 
 const sourceDirectory = args[0];
 const destinationDirectory = args[1];
-
-utils.walkSync(sourceDirectory)
-    .map(file => ({
-        filePath: file,
-        fileDatePath: utils.parseDate(constants.DATE_REGEX.exec(file) ? constants.DATE_REGEX.exec(file)[0] : "")
-    }))
-    .filter(file => file.fileDatePath)
-    .forEach(file => {
-        // creating directory strcutre
-        // moving files from src to dest
-        utils.mkDirByPathSync(`${path.resolve(destinationDirectory)}${path.sep}${file.fileDatePath}`);
-        utils.moveFiles(file.filePath, `${path.resolve(destinationDirectory)}${path.sep}${file.fileDatePath}${path.sep}${path.basename(file.filePath)}`);
-    });
+/* eslint-disable no-console */
+console.log(`Copying from source directory '${sourceDirectory}' to destination '${destinationDirectory}'`);
+/* eslint-enable no-console */
+const sourceFiles = utils.walkSync(sourceDirectory);
+const sourceFilesCount = sourceFiles.length;
+if (sourceFiles.length === 0) {
+  throw new Error('There are no files in source folder to copy');
+}
+/* eslint-disable no-console */
+console.log(`Starting to move ${sourceFilesCount} files`);
+/* eslint-enable no-console */
+sourceFiles
+  .map((file) => ({
+    filePath: file,
+    fileDatePath: utils.parseDate(constants.DATE_REGEX.exec(file) ? constants.DATE_REGEX.exec(file)[0] : ''),
+  }))
+  .filter((file) => file.fileDatePath)
+  .forEach((file, index) => {
+    // creating directory strcutre
+    utils.mkDirByPathSync(`${path.resolve(destinationDirectory)}${path.sep}${file.fileDatePath}`);
+    // moving files from src to dest
+    utils.moveFiles(
+      file.filePath,
+      `${path.resolve(destinationDirectory)}${path.sep}${file.fileDatePath}${path.sep}${path.basename(file.filePath)}`,
+      () => {
+        /* eslint-disable no-console */
+        console.log(`moving file ${index + 1} of ${sourceFilesCount}, ${file.filePath}`);
+        /* eslint-enable no-console */
+      },
+    );
+  });
